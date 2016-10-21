986cbbb977f39f646c685b6b0953a79c2ebb6e42
Scott Grogan <scott@ninjascript.com>
2016-10-21 14:56:34 -0700

    react-rails supports ES6!

--- app/assets/javascripts/components/todo_entry.js.jsx
+++ app/assets/javascripts/components/todo_entry.js.jsx
@@ -1,36 +1,39 @@
-var TodoEntry = React.createClass({
-  propTypes: {
-    name: React.PropTypes.string,
-    done: React.PropTypes.bool
-  },
+class TodoEntry extends React.Component {
 
-  getInitialState: function() {
-    return {
+  constructor (props) {
+    super(props)
+    this.state = {
       name: this.props.name,
-      done: this.props.done
-    };
-  },
-
-  getClassName: function () {
-    var className = 'todo_entry';
-    if (this.state.done == true) {
-      className += ' todo_entry--done';
+      done: this.props.done,
+      onClickHandler: this.complete.bind(this)
     }
-    return className;
-  },
+  }
 
-  render: function () {
+  getClassName () {
+    var className = 'todo_entry'
+    if (this.state.done == true) {
+      className += ' todo_entry--done'
+    }
+    return className
+  }
+
+  render () {
     return (
-      <li className={this.getClassName()}>
-        <button type="button" className="close close-inline-left" aria-label="Done" onClick={this.complete}>
+      <li className={ this.getClassName() }>
+        <button type="button" className="close close-inline-left" aria-label="Done" onClick={this.state.onClickHandler}>
           <span aria-hidden="true">&times;</span>
         </button>
-        {this.state.name}
+        { this.state.name }
       </li>
-    );
-  },
+    )
+  }
 
-  complete: function () {
+  complete () {
     this.setState({ done: true })
   }
-});
+}
+
+TodoEntry.propTypes = {
+  name: React.PropTypes.string,
+  done: React.PropTypes.bool
+}
--- app/assets/javascripts/components/todo_list.js.jsx
+++ app/assets/javascripts/components/todo_list.js.jsx
@@ -1,42 +1,45 @@
-var TodoList = React.createClass({
-  propTypes: {
-    title: React.PropTypes.string,
-    todo_entries: React.PropTypes.array
-  },
+class TodoList extends React.Component {
 
-  getInitialState: function() {
-    return {
+  constructor (props) {
+    super(props)
+    this.state = {
       title: this.props.title,
-      entries: this.props.todo_entries
-    };
-  },
+      entries: this.props.todo_entries,
+      onKeyDownHandler: this.addEntry.bind(this)
+    }
+  }
 
-  render: function() {
+  render () {
     return (
       <div>
-        <h2>{this.state.title}</h2>
+        <h2>{ this.state.title }</h2>
         <div className="form-group">
-          <input type="text" className="form-control" placeholder="Remember to..." onKeyDown={this.addEntry}/>
+          <input type="text" className="form-control" placeholder="Remember to..." onKeyDown={this.state.onKeyDownHandler}/>
         </div>
         <ul className="list-unstyled">
-          {this.renderEntries()}
+          { this.renderEntries() }
         </ul>
       </div>
-    );
-  },
+    )
+  }
 
-  renderEntries: function() {
+  renderEntries () {
     return this.state.entries.map(function (entry) {
       return <TodoEntry key={entry.name} name={entry.name} done={entry.done}/>
-    });
-  },
+    })
+  }
 
-  addEntry: function(evt) {
+  addEntry (evt) {
     if (evt.keyCode == 13) {
       this.setState({
         entries: this.state.entries.concat({ name: evt.target.value, done: false })
-      });
-      evt.target.value = '';
+      })
+      evt.target.value = ''
     }
   }
-});
+}
+
+TodoList.propTypes = {
+  title: React.PropTypes.string,
+  todo_entries: React.PropTypes.array
+}
