831b70ab4fa0df5ed2e8f82a32a4d98bfc145ba3
Scott Grogan <sgrogan@navigatingcancer.com>
2016-10-24 19:39:43 -0700

    Removing things that will be built during the demo

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,10 +1,4 @@
 class HomepageController < ApplicationController
   def index
-    @todo_list = TodoList.first.as_json({
-      only: [:id, :title],
-      include: {
-        entries: { only: [:id, :name, :done] }
-      }
-    })
   end
 end
